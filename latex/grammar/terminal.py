aeab5af58aa750e27600bfe14e64537e549dce41
iLykTurtlz <pjarski@sfcm.edu>
2024-12-01 13:22:46 -0800

    implemented Terminal .expand so that it's no longer abstractmethod

--- latex/grammar/terminal.py
+++ latex/grammar/terminal.py
@@ -3,9 +3,8 @@ from grammar.symbol import Symbol, SymbolFactory
 from abc import ABC, abstractmethod
 
 class Terminal(Symbol, ABC):
-    @abstractmethod
     def expand(self):
-        raise NotImplementedError("You must implement this method.")
+        return self
     
     def has_expanded(self):
         return self.value is not None
